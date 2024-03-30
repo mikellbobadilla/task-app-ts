6a7bb50810e65e667826d950099b18845c0d7276
sting <bobadillasting@gmail.com>
2024-03-30 02:57:05 -0300

    Add authSlice to store reducer

--- src/service/features/store/store.ts
+++ src/service/features/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authSlice from '../slices/authSlice'
 import taskSlice from '../slices/taskSlice'
 
 export const store = configureStore({
   reducer: {
     task: taskSlice,
+    auth: authSlice,
   },
 })
 
