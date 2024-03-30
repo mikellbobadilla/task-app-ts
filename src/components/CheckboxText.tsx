591c0731ed7eea14a0d444046055b9bcd8bb6e07
sting <bobadillasting@gmail.com>
2024-03-29 23:18:23 -0300

    Refactor CheckboxText component to accept additional props

--- src/components/CheckboxText.tsx
+++ src/components/CheckboxText.tsx
@@ -1,11 +1,29 @@
 import './CheckboxText.css'
 
-export function CheckboxText({ label }: { label: string }) {
+export function CheckboxText({
+  label,
+  isChecked,
+  onUpdate,
+  name,
+  id,
+}: {
+  label: string
+  isChecked?: boolean
+  onUpdate?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  name?: string
+  id?: string
+}) {
   return (
     <div className="flex items-center mb-5">
       <div className="checkbox-container">
-        <input type="checkbox" id="cb1" />
-        <label htmlFor="cb1">{label}</label>
+        <input
+          type="checkbox"
+          id={id}
+          name={name}
+          checked={isChecked}
+          onChange={onUpdate}
+        />
+        <label htmlFor={id}>{label}</label>
       </div>
     </div>
   )
