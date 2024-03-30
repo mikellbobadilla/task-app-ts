7b42e54482920394f3bd27fdec500a533030b1ad
sting <bobadillasting@gmail.com>
2024-03-29 23:18:17 -0300

    Add Redux store provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,14 @@ import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
 import '@fontsource/poppins/800.css'
 import '@fontsource/poppins/900.css'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
+import { store } from './service/features/store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
