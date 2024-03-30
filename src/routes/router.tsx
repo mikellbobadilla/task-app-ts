7d8e243196f7fb94677dacdc685ee8d613ec9d51
sting <bobadillasting@gmail.com>
2024-03-30 02:56:51 -0300

    Refactor router configuration

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,26 +1,31 @@
-import { createBrowserRouter, redirect } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import { ListOfTasks } from '../components/ListOfTasks'
 import { TaskForm } from '../components/TaskForm'
-import { IndexPage } from '../pages/IndexPage'
+import { LoginPage } from '../pages/LoginPage'
+import { MainPage } from '../pages/MainPage'
+import { ProtectedPages } from '../pages/ProtectedPage'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    loader: ({ request }) => {
-      const url = new URL(request.url)
-      if (url.pathname === '/') return redirect('/tasks')
-      return null
-    },
-    element: <IndexPage />,
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    element: <ProtectedPages />,
     children: [
       {
-        index: true,
-        path: 'tasks',
-        element: <ListOfTasks />,
-      },
-      {
-        path: 'create-task',
-        element: <TaskForm />,
+        path: '/',
+        element: <MainPage />,
+        children: [
+          {
+            index: true,
+            element: <ListOfTasks />,
+          },
+          {
+            path: 'create-task',
+            element: <TaskForm />,
+          },
+        ],
       },
     ],
   },
